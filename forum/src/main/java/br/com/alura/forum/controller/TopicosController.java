be6b755a315098b97c05620ed0d645780bac43c7
Wallison Ferreira <sanfer_wallison@hotmail.com>
2023-01-27 13:08:40 -0500

    feat: adds repository

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,22 +3,33 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDTO> lista() {
-        Topico topico = new Topico(
-                "Dúvidas",
-                "Dúvidas com Spring",
-                new Curso("Spring", "Programação"));
-
-        return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDTO.converter(topicos);
     }
+
+    // @RequestMapping("/topicos")
+    // public List<TopicoDTO> lista() {
+    // Topico topico = new Topico(
+    // "Dúvidas",
+    // "Dúvidas com Spring",
+    // new Curso("Spring", "Programação"));
+
+    // return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+    // }
 }
